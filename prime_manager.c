6e4a5bc2992bd8262c6f1bac70818a5df6c9d0cb
238962 <s238962@lak.ict.pwr.wroc.pl>
2019-04-01 17:07:59 +0200

    pliczki

--- prime_manager.c
+++ prime_manager.c
@@ -1,34 +1,58 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
-
+char output_file[] = "wynik.bin";
 
 int main(int argc, char *argv[]){
 
-    int threads = atoi(argv[1]);
-    int increment = 500;
+    int FROM = 0;
+    int TO = 100;
+    int range = TO - FROM;
 
+    int threads = 2;//atoi(argv[1]);
+    int increment = range/threads;
+    
     for(int i = 0; i < threads; i++) {
         if(fork() == 0) {
-            int from = i * increment;
+            int from = FROM + i * increment;
             int to = from + increment;
             char from_str[sizeof(int) * 4 + 1];
             char to_str[sizeof(int) * 4 + 1];
             sprintf(from_str, "%d", from);
             sprintf(to_str, "%d", to);
-            execl("./d", "d", from_str, to_str, (char *)0);
+            return execl("./single_prime", "single_prime", from_str, to_str, (char *)0);
     	}
     }
 
-    int total = 0;
     for (int thread = 0; thread < threads; thread++) {
         int status;
         wait(&status);
         int result = WEXITSTATUS(status);
-        total += result;
-        printf("Thread %d returned %d, total = %d\n", thread, result, total);
+        printf("Thread %d returned %d \n", thread, result);
     }
+
+    int total = 0;
+    int buffer[threads];
+    int ptr = open(output_file, O_RDONLY);  // r for read, b for binary
+
+    read(ptr, buffer, sizeof(buffer)); // read 10 bytes to our buffer
+    close(ptr);
+    for (int i = 0; i < threads; i++) {
+        printf("%d\n", buffer[i]);
+        total += buffer[i];
+    }
+    printf("Total: %d\n", total);
+    // int total = 0;
+    // for (int thread = 0; thread < threads; thread++) {
+    //     int status;
+    //     wait(&status);
+    //     int result = WEXITSTATUS(status);
+    //     total += result;
+    //     printf("Thread %d returned %d, total = %d\n", thread, result, total);
 }
--- single_prime.c
+++ single_prime.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+char output_file[] = "wynik.bin";
 
 
 int is_prime(int number){
@@ -20,6 +26,8 @@ int is_prime(int number){
 int main(int argc, char * argv[]) {
     int from = atoi(argv[1]);
     int to = atoi(argv[2]);
+    // int from = 1;
+    // int to = 100;
     int count = 0;
     
     for (int i = from; i < to; i++) {
@@ -27,6 +35,30 @@ int main(int argc, char * argv[]) {
             count++;
         }
     }
+
+    int handle = open(output_file, O_WRONLY | O_APPEND | O_CREAT, 0777);
+    if(handle < 0) {
+        perror("open");
+        return 0;
+    }
+    int lock;
+    lock = lockf(handle, F_LOCK, 0);
+    // printf("Lock: %d", lock);
+    if(lock < 0) {
+        perror("lockf");
+        return 0;
+    }
+    // sleep(2);
+    // printf("lock: %d\n", lock);
+    printf("Writing %d\n", count);
+    int wr = write(handle, &count, sizeof(int));
+    if(wr < 0) {
+        perror("write");
+    }
+    int release = lockf(handle, F_ULOCK, 0);
+    // printf("lock released: %d\n", release);
+    close(handle);
+    printf("od %d do %d liczb %d\n", from, to, count);
     
-    exit(count);
+
 }
