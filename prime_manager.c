1300534e040fa844aaedb63b6e7a45f06b28bbaf
238962 <s238962@mail.pwr.wroc.pl>
2019-05-06 17:04:12 +0200

    FIFO working

--- prime_manager.c
+++ prime_manager.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-char OUTPUT_FILE_NAME[] = "wynik.bin";
+char FIFO_NAME[] = "fifo";
 
 struct  {
     int from;
@@ -15,106 +15,61 @@ struct  {
     int count;
 } Report;
 
-int is_prime(int number){
-    if (number == 1 || number == 0) {
-        return 0;
-    }
-
-    for(int i = number - 1; i > 1; i--) {
-        if(number % i == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
 
 
-int count_primes(int from, int to) {
-    int count = 0;
-    for (int i = from; i < to; i++) {
-        if(is_prime(i)) {
-            count++;
-        }
-    }
-    return count;
+int run_single_prime(int from, int to) {
+    char from_str[sizeof(int) * 4 + 1];
+    char to_str[sizeof(int) * 4 + 1];
+    sprintf(from_str, "%d", from);
+    sprintf(to_str, "%d", to);
+    return execl("./single_prime", "single_prime", from_str, to_str, (char *)0);
 }
 
 int main(int argc, char *argv[]){
 
     int FROM = 0;
-    int TO = 100;
+    int TO = 5000;
     int range = TO - FROM;
   
-    int threads = 2;//atoi(argv[1]);
+    int threads = 4;//atoi(argv[1]);
     int increment = range/threads;
-    int pipe_files[2];      
-    pipe(pipe_files); 
 
-                int total = 0;
-    
-    for(int i = 0; i < threads; i++) {
-        int from = FROM + i * increment;
-        int to = from + increment;
-
-        if(fork() == 0) {  // potomny --------------
-            printf("Counting primes from %d to %d\n", from, to);
-            close(pipe_files[0]);
-            // ile liczb jest od FROM do TO
-            Report.from = from;
-            Report.to = to;
-            Report.count = count_primes(from, to);
-            printf("Counted %d primes from %d to %d\n", Report.count, from, to);
-
-            write(pipe_files[1], &Report, sizeof(Report));
-            close(pipe_files[1]);
-            return 0;
-            // int from = FROM + i * increment;
-            // int to = from + increment;
-            // char from_str[sizeof(int) * 4 + 1];
-            // char to_str[sizeof(int) * 4 + 1];
-            // sprintf(from_str, "%d", from);
-            // sprintf(to_str, "%d", to);
-            // return execl("./single_prime", "single_prime", from_str, to_str, (char *)0);
-    	} 
+    int total = 0;
+    remove(FIFO_NAME);
+    if(mkfifo(FIFO_NAME, S_IRUSR | S_IWUSR) < 0) {   
+        perror("Pipe");
+        return 0; 
     }
 
-    close(pipe_files[1]);
+    for(int i = 0; i < threads; i++) {
+
+        if(fork() == 0) {  // potomny --------------
+            int from = FROM + i * increment;
+            int to = from + increment;
+            return run_single_prime(from, to);
+    	} 
+    }
+  
+    int fifo = open(FIFO_NAME, O_RDWR);
+    if (fifo < 0) {
+        perror("Couldn't open FIFO");
+    }
+
     for (int thread = 0; thread < threads; thread++) {
         int status;
         wait(&status);
         int result = WEXITSTATUS(status);
         printf("Thread %d returned %d \n", thread, result);
-        read(pipe_files[0], &Report, sizeof(Report));
+      
+        read(fifo, &Report, sizeof(Report));
         printf("Od %d do %d liczb %d\n",Report.from,Report.to,Report.count); 
         total += Report.count;
         printf("Reported %d\n", Report.count);
     }
+
     printf("Total: %d\n", total);
+
+    close(fifo);
+    remove(FIFO_NAME);
     return total;    
 }
-    
-
-    // for (int thread = 0; thread < threads; thread++) {
-    //     int status;
-    //     wait(&status);
-    //     int result = WEXITSTATUS(status);
-    //     printf("Thread %d returned %d \n", thread, result);
-    // }
-
-    // struct Report report;
-    // int total = 0;
-    // int output_file = open(OUTPUT_FILE_NAME, O_RDONLY);
-    // if(output_file < 0) {
-    //     perror("Could not open output file");
-    // }
-
-    // for (int i = 0; i < threads; i++) {
-    //     int r = read(output_file, &report, sizeof(report));
-    //     printf("%d\n", report.count);
-    //     total += report.count;
-    // }
-
-    // close(output_file);
-
-    // printf("Total: %d\n", total);
-
--- single_prime.c
+++ single_prime.c
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-char output_file[] = "wynik.bin";
+char FIFO_NAME[] = "fifo";
 
 
 int is_prime(int number){
@@ -42,41 +42,31 @@ struct Report {
 
 
 int main(int argc, char * argv[]) {
-    // int from = atoi(argv[1]);
-    // int to = atoi(argv[2]);
     struct Report report;
 
     report.from = atoi(argv[1]);
     report.to = atoi(argv[2]);
     report.count = count_primes(report.from, report.to);
 
-    int handle = open(output_file, O_WRONLY | O_APPEND | O_CREAT, 0777);
+    int handle = open(FIFO_NAME, O_WRONLY | O_APPEND | O_CREAT, 0777);
     if(handle < 0) {
         perror("open");
         return 0;
     }
+
     int lock;
     lock = lockf(handle, F_LOCK, 0);
-    // printf("Lock: %d", lock);
     if(lock < 0) {
         perror("lockf");
         return 0;
     }
-    // sleep(2);
-    // printf("lock: %d\n", lock);
+
     printf("Writing %d\n", report.count);
     int wr = write(handle, &report, sizeof(report));
     if(wr < 0) {
         perror("write");
     }
-    int release = lockf(handle, F_ULOCK, 0);
-    // printf("lock released: %d\n", release);
-    close(handle);
     printf("od %d do %d liczb %d\n", report.from, report.to, report.count);
-    
-    struct Report report2;
-    int reopen = open(output_file, O_RDONLY);
-    read(reopen, &report2, sizeof(report2));
-    printf("od %d do %d liczb %d\n", report2.from, report2.to, report2.count);
-    close(reopen);
+    int release = lockf(handle, F_ULOCK, 0);
+    close(handle);
 }
