cfc5f081adb553d349d7c5e1716782efa00f12d3
238962 <s238962@mail.pwr.wroc.pl>
2019-06-10 16:07:51 +0200

    producer_threads

--- producer.c
+++ producer.c
@@ -1,28 +1,92 @@
-#include <sys/mman.h> 
-#include <fcntl.h> 
-#include <semaphore.h> 
-#include <stdio.h> 
-#include <unistd.h> 
-#include <sys/types.h> 
-#include <sys/wait.h> 
-#include "common.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(int argc, char **argv)
+///////////
+#define SIZE 4 // Liczba pozycji (napisow) w buforze
+#define LSIZE 80 // Dlugosc napisu
+char buf[SIZE][LSIZE]; // Bufor na napisy
+int head = 0; // Tutaj wpisujemy do bufora nowy element
+int tail = 0; // Stad pobieramy element z bufora
+int cnt = 0; // Liczba elementow w buforze
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER; // Tu czekamy gdy brak miejsca w buforze
+pthread_cond_t full = PTHREAD_COND_INITIALIZER; // Tu czekamy gdy brak rekordow w buforze
+pthread_mutex_t mutex; // Wzajemne wykluczanie
+////////////
+
+int wynik;
+
+void* consume(void* arg)
 {
-    bufor_t* wbuf = init_buff();
+    while (1) {
+        pthread_mutex_lock(&mutex);
 
-    for(int i=0; i<10; i++) {
-       sem_wait(&(wbuf->empty)); 
-       sem_wait(&(wbuf->mutex)); 
-       sprintf(wbuf->buf[wbuf->head],"Komunikat %d",i);  
-       printf("Producent - cnt:%d head: %d tail: %d\n",  
-               wbuf-> cnt,wbuf->head,wbuf->tail); 
-       wbuf-> cnt ++; 
-       wbuf->head = (wbuf->head +1) % BSIZE; 
-       sem_post(&(wbuf->mutex)); 
-       sem_post(&(wbuf->full)); 
-       sleep(1);
+        while (cnt == 0) {
+            printf("Buffer empty\n");
+            pthread_cond_wait(&empty, &mutex);
+        }
+        printf("Consumed \n");
+        printf(buf[tail]);
+        if (tail == SIZE - 1) {
+            tail = 0;
+        }
+        else {
+            tail++;
+        }
+        cnt--;
+        pthread_cond_signal(&full);
+        pthread_mutex_unlock(&mutex);
+        sleep(rand() % 2);
     }
+    pthread_exit((void*)&wynik);
+}
+
+void* produce(void* arg)
+{
+    int i = 0;
+    while (1) {
+        pthread_mutex_lock(&mutex);
+
+        while (cnt == SIZE) {
+            printf("Buffer full\n");
+            pthread_cond_wait(&full, &mutex);
+        }
+        printf("Produced\n");
+        sprintf(buf[head], "Producer %d, step %d\n", (int) arg, i);
+        if (head == SIZE - 1) {
+            head = 0;
+        }
+        else {
+            head++;
+        }
+        cnt++;
+        pthread_cond_signal(&empty);
+
+        pthread_mutex_unlock(&mutex);
+        i++;
+        sleep(rand() % 2);
+    }
+    pthread_exit((void*)&wynik);
+}
+
+int main(int argc, char* argv[])
+{
+    pthread_t producer_1_id = 1;
+    pthread_t producer_2_id = 2;
+    pthread_t consumer_id = 3;
+    int i, status;
+    void** statp;
+    printf("HI");
+
+    pthread_create(&producer_1_id, NULL, produce, (void*)(producer_1_id));
+    pthread_create(&producer_2_id, NULL, produce, (void*)(producer_2_id));
+    pthread_create(&consumer_id, NULL, consume, (void*)(consumer_id));
+
+    pthread_join(producer_1_id, (void*)&statp);
+    pthread_join(producer_2_id, (void*)&statp);
+    pthread_join(consumer_id, (void*)&statp);
 
     return 0;
 }
--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
-gcc consumer.c -o consumer -lrt -lpthread
+# gcc consumer.c -o consumer -lrt -lpthread
 gcc producer.c -o producer -lrt -lpthread
-gcc init.c -o init -lrt -lpthread
+# gcc init.c -o init -lrt -lpthread
